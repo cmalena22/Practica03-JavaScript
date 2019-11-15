4f9e78830c5f106fd4ae52b74eebf2f467fe610b
Cinthia Iza <cizac1@est.ups.edu.ec>
2019-11-15 09:08:50 -0500

    Creacion de PHP  para cuando pase las validaciones

---
+++ formulario/bien.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Nuevo Usuario</title>
+		
+		<link rel="stylesheet" type="text/css" href="../formulario/formulario.css">
+		
+	</head>
+	<body>
+		<?php
+			echo "<h1>Bienvenido, pasaste las validaciones!</h1>";
+		?>
+	</body>
+</html>
