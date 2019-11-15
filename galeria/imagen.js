b278e6bf53eef8b18fbba595320f41b3d65cca32
Cinthia Iza <cizac1@est.ups.edu.ec>
2019-11-15 09:11:13 -0500

    Creacion de JavaScript para las validaciones de la galeria

--- galeria/imagen.js
+++ galeria/imagen.js
@@ -1,19 +1,54 @@
-var ima=['imagenes/foto1.jpg','imagenes/foto2.jpg','imagenes/foto3.jpg',
-        'imagenes/foto4.jpg','imagenes/foto5.jpg','imagenes/foto6.png',
-        'imagenes/foto7.jpg','imagenes/foto8.jpg','imagenes/foto9.jpg',
-        'imagenes/foto10.jpg'];
+var ima=['../imagenes/foto1.jpg','../imagenes/foto2.jpg','../imagenes/foto3.jpg',
+        '../imagenes/foto4.jpg','../imagenes/foto5.jpg','../imagenes/foto6.png',
+        '../imagenes/foto7.jpg','../imagenes/foto8.jpg','../imagenes/foto9.jpg',
+        '../imagenes/foto10.jpg'];
 
-var ram= Math.floor(Math.random()*ima.length);
+//var ram= Math.floor(Math.random()*ima.length);
 var A=[]
-for (var i = 0; i <=4; i++) {
-    A[i]=Math.floor(Math.random()*ima.length);;
-   
-    console.log(A) 
+num=0;
+for ( i = 0; i <=4; i++) {
+  A[i]=ima[Math.floor(Math.random()*ima.length)];
+   console.log(A) 
+  console.log(i)
 }
 
-function iniciar(){
-    document.getElementById("imagen").src=ima[ram];
+function iniciar(){  
+ 
+  document.getElementById("imagen").src=A[0];
+
 }
+
+
+
+function siguiente (){
+ 
+num++;
+if(num>=A.length)
+{
+  num=0;
+}
+    document.getElementById("imagen").src=A[num];    
+ 
+}
+
+function atras (){
+
+  num--;
+  if(num>A.length-1)
+  {
+       
+num=0;
+     
+  }
+        document.getElementById("imagen").src=A[num]; 
+
+  b=A.length
+  console.log('dhr')
+  console.log(b)
+
+    
+  }
+  
 
 //function siguiente(){
   //  var getRandomNumber = Math.floor(Math.random()*5);
